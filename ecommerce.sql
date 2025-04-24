6854f2a61e7370fac55edf160ef7f412b5cdcb49
Kipngetich Josphat <joskipngetich07@gmail.com>
2025-04-24 22:56:09 +0300

    updated the file with sql code for these tables brand,product,size category and product variation

--- ecommerce.sql
+++ ecommerce.sql
@@ -25,3 +25,37 @@ CREATE TABLE product_category (
     category_id INT PRIMARY KEY AUTO_INCREMENT,
     category_name VARCHAR(50) UNIQUE
 );
+
+-- brand Table
+CREATE TABLE brand (
+    brand_id INT PRIMARY KEY AUTO_INCREMENT,
+    brand_name VARCHAR(50) UNIQUE
+);
+
+
+-- product Table
+CREATE TABLE product (
+    product_id INT PRIMARY KEY AUTO_INCREMENT,
+    brand_id INT,
+    category_id INT,
+    product_name VARCHAR(255),
+    base_price DECIMAL(10, 2),
+    FOREIGN KEY (brand_id) REFERENCES brand(brand_id),
+    FOREIGN KEY (category_id) REFERENCES product_category(category_id)
+);
+
+
+-- product_variation Table
+CREATE TABLE product_variation (
+    variation_id INT PRIMARY KEY AUTO_INCREMENT,
+    product_id INT,
+    variation_name VARCHAR(50), -- E.g., 'Size', 'Color'
+    FOREIGN KEY (product_id) REFERENCES product(product_id)
+);
+
+
+-- size_category Table
+CREATE TABLE size_category (
+    size_category_id INT PRIMARY KEY AUTO_INCREMENT,
+    category_name VARCHAR(50) UNIQUE -- E.g., 'Clothing', 'Shoes'
+);
